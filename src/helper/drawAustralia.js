b406f12c24c36ffa5486936d6fc6345f64c507d7
Stanley Huang <infamous_godhand@yahoo.com>
2018-11-25 02:32:29 +0000

    added hover change color and tooltip

--- src/helper/drawAustralia.js
+++ src/helper/drawAustralia.js
@@ -1,6 +1,31 @@
 import * as d3 from "d3";
 import * as topojson from "topojson";
+import d3tip from "d3-tip";
 import geoData from "../geoData/aus_state_topojson";
+
+const mouseOver = (d, i, n) => {
+  d3.select(n[i])
+    .transition("changeSliceFill")
+    .duration(300)
+    .attr("fill", "red");
+};
+
+const mouseOut = (d, i, n) => {
+  //console.log("out");
+  d3.select(n[i])
+    .transition()
+    .duration(300)
+    .attr("fill", "blue");
+};
+
+const tip = d3tip()
+  .attr("class", "tip card")
+  .html(d => {
+    //console.log(d.data.cost);
+    let content = `<div class="name">${d.properties.STATE_NAME}</div>`;
+    console.log(d.properties);
+    return content;
+  });
 
 const draw = svgRef => {
   //console.log("drawing australia");
@@ -27,6 +52,8 @@ const draw = svgRef => {
 
   const graph = svg.select("#australia");
 
+  graph.call(tip);
+
   graph
     .selectAll("path")
     .data(geojson.features)
@@ -35,6 +62,17 @@ const draw = svgRef => {
     .attr("d", path)
     .attr("stroke", "dimgray")
     .attr("fill", "blue");
+
+  graph
+    .selectAll("path")
+    .on("mouseover", (d, i, n) => {
+      mouseOver(d, i, n);
+    })
+    .on("mouseout", (d, i, n) => {
+      tip.hide();
+      mouseOut(d, i, n);
+    })
+    .on("click", (d, i, n) => tip.show(d, n[i]));
 };
 
 export default draw;
