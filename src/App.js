936c111af64b33ebaad6e9e716b031b17517cb3c
Stanley Huang <infamous_godhand@yahoo.com>
2018-11-30 04:19:57 +0000

    done, added data reference

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 //import "./App.css";
 import Header from "./components/header";
 import ChartContainer from "./components/chartContainer";
+import Reference from "./components/reference";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Header />
         <ChartContainer />
+        <Reference />
       </div>
     );
   }
---
+++ src/components/reference.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Reference = () => {
+  return (
+    <div className="footer-copyright">
+      <div class="container">
+        <h4>References</h4>
+        <p>
+          Australian Bureau of Statistics. Australian Health Survey 2014/15
+          (4364.0)
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Reference;
